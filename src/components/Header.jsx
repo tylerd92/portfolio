40a98d0a6adf14635bc21e60c2374c3766a677a1
tylerd92 <twdeans@gmail.com>
2025-08-13 01:52:39 -0400

    create about page

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -4,7 +4,9 @@ const Header = () => {
   return (
     <header className='bg-white shadow-md sticky top-0 z-50'>
       <div className='max-w-7xl mx-auto px-4 py-4 flex justify-between items-center'>
-        <h1 className='text-2xl font-bold'>Tyler Deans</h1>
+        <Link to='/' className='text-2xl font-bold hover:text-blue-500'>
+          Tyler Deans
+        </Link>
         <nav className='space-x-4'>
           <Link to='about' className='hover:text-blue-500'>
             About
--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -4,6 +4,26 @@ const Projects = () => {
       <h2 className='text-3xl font-semibold mb-6'>Featured Projects</h2>
       <div className='grid md:grid-cols-2 gap-6'>
         <div className='bg-white shadow rounded-xl p-5'>
+          <h3 className='text-xl font-semibold'>Calculator App</h3>
+          <p className='text-sm text-gray-600 mt-2'>
+            A calculator application built with Python, FastAPI, JavaScript, TailwindCSS, and HTML. Users can create an account and perform basic arithmetic operations.
+          </p>
+          <a
+            href='https://github.com/tylerd92/calculator_final_project'
+            className='inline-block mt-3 text-blue-600 hover:underline'
+            target="_blank"
+          >
+            GitHub
+          </a>
+          <a
+            href='https://calculator-app-zdtd.onrender.com/'
+            className='inline-block ml-3 text-blue-600 hover:underline'
+            target="_blank"
+          >
+            Live Demo
+          </a>
+        </div>
+        <div className='bg-white shadow rounded-xl p-5'>
           <h3 className='text-xl font-semibold'>Chat App</h3>
           <p className='text-sm text-gray-600 mt-2'>
             A real-time chat application built with Node.js, Express, and Socket.io (web sockets).
@@ -24,18 +44,6 @@ const Projects = () => {
             Live Demo
           </a>
         </div>
-        {/* <div className='bg-white shadow rounded-xl p-5'>
-          <h3 className='text-xl font-semibold'>Calculator App</h3>
-          <p className='text-sm text-gray-600 mt-2'>
-            TBD
-          </p>
-          <a
-            href='#'
-            className='inline-block mt-3 text-blue-600 hover:underline'
-          >
-            Live Demo
-          </a>
-        </div> */}
       </div>
     </section>
   );
